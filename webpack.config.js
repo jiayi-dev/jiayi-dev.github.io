7f98f7a279cdee2b0a2e2dd74263c2919fe613e2
jiayi-dev <li2646@mcmaster.ca>
2024-12-28 15:36:15 -0500

    Update the Webpack config to handle serving static files from the public folder, referring to the images using the public path.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -28,9 +29,29 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'public',
+                    to: '',
+                    globOptions:{
+                        ignore: ['**/index.html'],
+                    },
+                },
+            ],
+        }),
     ],
     devServer: {
-        static: path.join(__dirname, 'dist'),
+        static: [
+            {
+                directory: path.join(__dirname, 'dist'),
+                publicPath: '/',
+            },
+            {
+                directory: path.join(__dirname, 'public'),
+                publicPath: '/',
+            }
+        ],
         open: true,
         hot: true,
         port: 8080,
